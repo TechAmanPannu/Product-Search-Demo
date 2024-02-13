e25d95710f83abd89240783a618d17edd7c8bb72
Amandeep Singh Pannu <amandeepsingh.pannu@loblaw.ca>
2024-02-13 14:57:31 -0500

    Optimizations

--- src/main/java/com/product/search/controller/ProductSearchController.java
+++ src/main/java/com/product/search/controller/ProductSearchController.java
@@ -4,10 +4,7 @@ import com.product.search.entity.Product;
 import com.product.search.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,8 @@ public class ProductSearchController {
 
     private final ProductService productService;
 
-    @GetMapping("/search-by-liam")
-    public ResponseEntity<Product> searchProducts(@RequestParam(value = "liam", required = true) String liam) {
+    @GetMapping("/liams/{liamId}")
+    public ResponseEntity<Product> getByLiam(@PathVariable(value = "liamId", required = true) String liam) {
         return ResponseEntity
                 .ok(productService.getByLiam(liam));
     }
